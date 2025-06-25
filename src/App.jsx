bd4fa72ed70976b0ba7abd8b0c02901aa987b062
aleavy <aleavy02@gmail.com>
2025-06-25 15:30:41 -0300

    cambie de idea jaja

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import { MainPage } from './MainPage'
+
+
 
 
 function App(){
   return(
-    <>
-      <MainPage />
-    </>
+    <main className="max-h-12 rounded-[2rem] bg-amber-100 px-24 pb-48 pt-16">
+      <header className="">
+        <nav className="flex justify-between py-3">
+          <div>
+            <a className="" href="#">
+              <img className="max-h-12 w-full h-full object-cover" src="./book-svgrepo-com.svg" alt="book" />
+            </a>
+          </div>
+          <div>
+            <a href="">Login</a>
+            <a href="">Register</a>
+            <a href="">Feature</a>
+          </div>
+        </nav>
+      </header>
+      <section>
+        <div>
+          <h2>Controla tu Libro</h2>
+          <p>Tatatatatatatatatat,</p>
+          <p>Tatatat.</p>
+          <button></button>
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/Btn.jsx
+++
@@ -1,7 +0,0 @@
-export function Btn({ name, url }){
-    return( 
-        <li className="flex bg-slate-900 hover:bg-slate-800 px-2 py-1 rounded-full grow border-1 border-gray-300 justify-items-center content-center">
-            <a className="flex-1 items-center justify-center content-center" href={url}>{name}</a>
-        </li>
-    )
-}
